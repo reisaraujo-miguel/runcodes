7f524d409246f05de1fe43a2308ac5eaf170eb6b
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-12-27 17:27:09 -0300

    refactor: use go-chi jwt middleware

--- backend/utils/jwt.go
+++ backend/utils/jwt.go
@@ -3,48 +3,14 @@ package utils
 
 import (
 	"os"
-	"time"
 
-	"github.com/golang-jwt/jwt/v5"
+	"github.com/go-chi/jwtauth/v5"
 )
 
 var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
-type Claims struct {
-	UserID string `json:"user_id"`
-	Email  string `json:"email"`
-	jwt.RegisteredClaims
-}
+var TokenAuth *jwtauth.JWTAuth
 
-func GenerateToken(userID, email string) (string, error) {
-	expirationTime := time.Now().Add(24 * time.Hour)
-
-	claims := &Claims{
-		UserID: userID,
-		Email:  email,
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(expirationTime),
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-			Issuer:    "runcodes",
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString(jwtSecret)
-}
-
-func ValidateToken(tokenString string) (*Claims, error) {
-	claims := &Claims{}
-	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, jwt.ErrSignatureInvalid
-		}
-		return jwtSecret, nil
-	})
-
-	if err != nil || !token.Valid {
-		return nil, err
-	}
-
-	return claims, nil
+func init() {
+	TokenAuth = jwtauth.New("HS256", jwtSecret, nil) // replace with secret key
 }
