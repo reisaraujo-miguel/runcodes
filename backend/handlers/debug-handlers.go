82ef72e51cae9ebef7cb53d7ca1cf52dcbf68886
Miguel R. Araújo (Leug) <58122451+reisaraujo-miguel@users.noreply.github.com>
2026-03-12 10:56:22 -0300

    Update backend/handlers/debug-handlers.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- backend/handlers/debug-handlers.go
+++ backend/handlers/debug-handlers.go
@@ -8,13 +8,18 @@ import (
 )
 
 func GenerateDebugToken(w http.ResponseWriter, r *http.Request) {
+	if os.Getenv("HOST") != "development" {
+		utils.WriteResponse(w, http.StatusNotFound, false, "not found", nil)
+		return
+	}
+
 	_, token, err := utils.TokenAuth.Encode(map[string]any{"user": 123})
 	if err != nil {
 		slog.Error(err.Error())
 		utils.WriteResponse(w, http.StatusInternalServerError, false, "an error ocurred", err)
 		return
 	}
-	slog.Info("A debug token is", slog.String("token", token))
 
 	utils.WriteResponse(w, http.StatusAccepted, true, "Your debug token is", map[string]string{"token": token})
+}
 }
