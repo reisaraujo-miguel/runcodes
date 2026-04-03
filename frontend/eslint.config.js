06e4890ada3db751861311965ea6cae9bbc7e0dd
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2026-04-03 20:42:32 -0300

    chore(front): some config fixes

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -14,7 +14,7 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs.flat["recommended-latest"],
       reactRefresh.configs.vite,
       tseslint.configs.recommendedTypeChecked,
       tseslint.configs.strictTypeChecked,
