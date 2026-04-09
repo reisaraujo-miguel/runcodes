2a06abba9b52b7e6baac4e0481d39a2bfb8c7f4b
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2026-04-09 08:33:00 -0300

    fix(back): docstring and error handling

--- backend/handlers/write-response.go
+++ backend/handlers/write-response.go
@@ -2,16 +2,19 @@ package handlers
 
 import (
 	"encoding/json"
+	"log/slog"
 	"net/http"
 )
 
 /*
-WriteResponse encodes a models.Response and writes using the provided http.ResponseWriter
+WriteResponse encodes a http response and writes it
 */
 func WriteResponse(w http.ResponseWriter, httpStatus int, data any) {
 	w.Header().Set("Content-Type", "application/json")
 
 	w.WriteHeader(httpStatus)
 
-	json.NewEncoder(w).Encode(data)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		slog.Error("failed to encode response", "error", err)
+	}
 }
